5117736be398b1b25b016bfec5987f6431a9757a
zhushan <605185419@qq.com>
2016-08-31 16:02:09 +0800

    播放时间和总时长

--- ZSPlayer/ZSPlayer.m
+++ ZSPlayer/ZSPlayer.m
@@ -19,6 +19,16 @@
 @property(nonatomic,strong)UIButton *fullbtn;
 //播放暂停按钮
 @property(nonatomic,strong)UIButton *playbtn;
+//播放时长
+@property(nonatomic,strong)UILabel *timedurtion;
+//当前播放时间
+@property(nonatomic,strong)UILabel *currenttime;
+//定时器
+@property(nonatomic,strong)NSTimer *timer;
+//播放的进度
+@property(nonatomic,strong)UISlider *progress;
+//已下载的进度
+@property(nonatomic,strong)UISlider *playableProgress;
 @end
 @implementation ZSPlayer
 
@@ -27,6 +37,7 @@
     if (self) {
         [self createMoviePlayer];
         [self setupUI];
+        [self addNotification];
     }
     return self;
 }
@@ -42,6 +53,7 @@
     [self.movieplayer prepareToPlay];
 }
 
+
 //设置UI
 -(void)setupUI{
     UIView *coverView = [[UIView alloc]initWithFrame:self.bounds];
@@ -56,9 +68,18 @@
     [self.coverView addSubview:fullbtn];
     
     self.playbtn = [[UIButton alloc]initWithFrame:CGRectMake(10, myH - 40, 50, 30)];
-    [_playbtn setTitle:@"播放" forState:UIControlStateNormal];
+    [_playbtn setTitle:@"暂停" forState:UIControlStateNormal];
     [_playbtn addTarget:self action:@selector(playorpauseBtnclick) forControlEvents:UIControlEventTouchUpInside];
     [self.coverView addSubview:_playbtn];
+    
+    self.timedurtion = [[UILabel alloc]initWithFrame:CGRectMake(myW - 140, myH - 40, 80, 30)];
+    self.timedurtion.textColor = [UIColor whiteColor];
+    [self.coverView addSubview:_timedurtion];
+    
+    self.currenttime = [[UILabel alloc]initWithFrame:CGRectMake(myW - 200, myH - 40, 60, 30)];
+    self.currenttime.textColor = [UIColor whiteColor];
+    [self.coverView addSubview:_currenttime];
+
 }
 
 //播放暂停
@@ -97,6 +118,29 @@
     [self.coverView layoutSubviews];
     [self layoutSubviews];
 }
+
+//添加通知
+-(void)addNotification{
+    NSNotificationCenter *notificationCenter=[NSNotificationCenter defaultCenter];
+    [notificationCenter addObserver:self selector:@selector(DurationAvailable) name:MPMovieDurationAvailableNotification object:self.movieplayer];
+    [notificationCenter addObserver:self selector:@selector(mediaPlayerPlaybackFinished) name:MPMoviePlayerPlaybackDidFinishNotification object:self.movieplayer];
+}
+
+- (void)DurationAvailable {
+    NSInteger minit = self.movieplayer.duration / 60;
+    NSInteger second = self.movieplayer.duration - 60 * minit;
+    self.timedurtion.text = [NSString stringWithFormat:@"%02ld:%02ld", (long)minit, (long)second];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(refreshCurrentTime) userInfo:nil repeats:YES];
+}
+
+- (void)refreshCurrentTime {
+    NSInteger minit = self.movieplayer.currentPlaybackTime / 60;
+    NSInteger second = self.movieplayer.currentPlaybackTime - 60 * minit;
+    self.currenttime.text = [NSString stringWithFormat:@"%02ld:%02ld", (long)minit, (long)second];
+    self.progress.value = self.movieplayer.currentPlaybackTime / self.movieplayer.duration;
+    self.playableProgress.value = self.movieplayer.playableDuration / self.movieplayer.duration;
+}
+
 
 
 //响应超出控件的按钮点击事件
